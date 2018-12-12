f47527e871570d5a525cd21834a4dd4848f9c893
Ashinator <Ahart425@gmail.com>
2018-12-11 21:56:12 -0700

    little victories

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,86 @@
-// Have player press any key to begin
+// Have player press any key to begin (did not work....)
 $(document).ready(function() {
-// VARIABLES DEFINED
-var computerNumber = [] 
+
+        // VARIABLES DEFINED
+        // computer number must be between 19-120
+    var computerNumber = [] 
     for (var i = 12; i <= 120; i++) {
         computerNumber.push(i)
-    
-    }
- userScore =0;
-
-randomNumber = computerNumber [Math.floor(Math.random() * computerNumber.length)]; 
-
-redcrystal = 0;
-bluecrystal = 0;
-yellowcrystal = 0;
-greencrystal = 0;
-
-
-// Start of game - Computer picks random number to match, assigns crystals different values
-
-// Press any key to begin
-document.onkeyup = function startGame() {
-    randomNumber = computerNumber [Math.floor(Math.random() * computerNumber.length)];  // computer puts random number on board
-     console.log(randomNumber); //SUCCESS!
-
-    for (var i = 0; i < randomNumber; i++); 
-
-    
         
+    }
 
-     // document.getElementById(randomNumber).append //HELP 
-    //   ("computerNumber");
+    userScore =0;
+    wins = 0;
+    losses = 0;
 
+    // computer picks random number 19-120
+    randomNumber = computerNumber [Math.floor(Math.random() * computerNumber.length)]; 
+
+    redcrystal = 0;
+    bluecrystal = 0;
+    yellowcrystal = 0;
+    greencrystal = 0;
+
+
+
+
+    // Supposed to press key to begin
+    // document.onkeyup = function startGame() {
+    randomNumber = computerNumber [Math.floor(Math.random() * computerNumber.length)];  // computer puts random number on board
+    console.log(randomNumber); //SUCCESS!
+
+                
+    // Start of the game - 
     roundStart = function() {
         
-         redcrystal, bluecrystal, greencrystal, yellowcrystal = 
-         Math.floor(Math.random() * 12) + 1;
-    } 
-                         
-         
-         roundStart();
+        bluecrystal = 
+        Math.floor(Math.random() * 12) + 1;
+        redcrystal = 
+        Math.floor(Math.random() * 12) + 1;
+        greencrystal = 
+        Math.floor(Math.random() * 12) + 1;
+        yellowcrystal = 
+        Math.floor(Math.random() * 12) + 1;
+    } ;
+                        
+                
+    roundStart();
+        
 
-       $("#red").click(function () {
-           userScore = userScore + redcrystal;
-           $("currentScore").innerHTML(" " + userScore);
-           
-       });
-          
-        console.log(redcrystal, bluecrystal, yellowcrystal, greencrystal);
-                      
-    }; // end Start Game Function
+        
+    console.log(redcrystal, bluecrystal, yellowcrystal, greencrystal); // SUCCESS
+                    
+    // end Start Game Function
+    
+    $("#computerNumber").append(randomNumber); // keeps adding the numbers together when pressing a key - need to RESTART everytime and clear previous number
+
+
+     $("#red").on("click", function() {
+        
+        userScore = userScore + redcrystal;
+        $("#currentScore").text(userScore)
+    })
+
+    $("#blue").on("click", function() {
+        userScore = userScore + bluecrystal;
+        $("#currentScore").text(userScore)
+    })
+
+    $("#green").on("click", function() {
+        userScore = userScore + greencrystal;
+        $("#currentScore").text(userScore)
+    })
+
+    $("#yellow").on("click", function() {
+        userScore = userScore + yellowcrystal;
+         $("#currentScore").text(userScore)
+    })
+                                
+                
+            
+    console.log(userScore); // Shows as 0 - but at least it is showing up now :)
+
+    $("#currentScore").append(userScore);
 
     // How to Win - User Guesses must equal Current number to Match
 
@@ -59,16 +91,17 @@ document.onkeyup = function startGame() {
         roundStart();
         console.log(losses);
     }
-     if (userScore === computerNumber) {
-         alert("You did it, great job! ");
-         wins = wins +1;
-         $("#wins").text(wins);
-         roundStart();
-         console.log(wins);
-     }
+    if (userScore === computerNumber) {
+        alert("You did it, great job! ");
+        wins = wins +1;
+        $("#wins").text(wins);
+        roundStart();
+        console.log(wins);
 
-    
-    }); // closes ALL functions
+
+    }
+}); // Closing of On Ready
+    // });
     
         
 
